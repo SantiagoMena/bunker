bf3f377a9e8d7db97e15fadeb08a633b5a94bc60
Santiago Mena <santiagomenape@gmail.com>
2023-10-07 11:33:08 -0300

    Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CompaniesModule } from './companies/companies.module';
+import { EmployeesModule } from './employees/employees.module';
+import {Company} from "./companies/entities/company.entity";
+import {Employee} from "./employees/entities/employee.entity";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [Company, Employee],
+      synchronize: true,
+    }),
+    CompaniesModule,
+    EmployeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
